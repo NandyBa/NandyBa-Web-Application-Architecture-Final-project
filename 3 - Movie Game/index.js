b6023821969cf54571e0021075743e60b7f17b42
Nandy Bâ <nandyba@yahoo.fr>
2021-03-24 23:36:19 +0100

    3 Etapes 7 et 8

--- 3 - Movie Game/index.js
+++ 3 - Movie Game/index.js
@@ -39,7 +39,19 @@ fetch(URL)
 						imgurl = "https://www.themoviedb.org/t/p/w220_and_h330_face" + person.profile_path;
 					}
 				})
-				document.getElementById('actor-info').innerHTML = '<img src="'+ imgurl +'"/><span class="name">' + response +'</span>';
+				var $actor_info = document.getElementById('actor-info');
+				$actor_info.innerHTML = '<img src="'+ imgurl +'"/><span class="name">' + response +'</span>';
+				$form_div = document.createElement("div");
+				$form = document.createElement("form");
+				$form_div.append($form);
+				$form.classList.id = 'form-movie';
+				$form.innerHTML = '\
+					<label for="form-movie">In what other movie this actor / actrice plays ?</label>\
+					<input id="form-movie" type="text" placeholder="Movie name">\
+					<input type="submit" value="Vérifier">\
+				';
+				$actor_info.parentNode.insertBefore($form_div, $actor_info.nextSibling);
+
 			}else{
 				document.getElementById('response').innerHTML = "incorrect";
 			}
