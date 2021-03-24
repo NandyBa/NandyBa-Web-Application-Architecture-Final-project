46e5fc6c079742ff38544fce21d20cbcb3fa0e7c
Nandy Bâ <nandyba@yahoo.fr>
2021-03-24 23:16:31 +0100

    3 Etape 6

--- 3 - Movie Game/index.js
+++ 3 - Movie Game/index.js
@@ -1,7 +1,7 @@
 
 API_Key = '12eefe636084e06b2430cf9b2a084aff';
 
-movie_id = 550
+movie_id = 550;
 
 URL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_Key}&language=en-US`;
 
@@ -27,14 +27,21 @@ fetch(URL)
 				actors.push(person.name.toLowerCase());
 			}
 		})
+		console.log(credits);
 		document.getElementById('form-who-play').addEventListener("submit", function(e){
 			e.preventDefault();
 			response = document.getElementById('form-actor').value.toLowerCase();
 
 			if(actors.includes(response)){
-				alert('correct')
+				var imgurl;
+				credits.cast.forEach(person =>{
+					if(person.name.toLowerCase() == response.toLowerCase()){
+						imgurl = "https://www.themoviedb.org/t/p/w220_and_h330_face" + person.profile_path;
+					}
+				})
+				document.getElementById('actor-info').innerHTML = '<img src="'+ imgurl +'"/><span class="name">' + response +'</span>';
 			}else{
-				document.getElementById('response').innerHTML = "incorrect"
+				document.getElementById('response').innerHTML = "incorrect";
 			}
 		});
 	})
